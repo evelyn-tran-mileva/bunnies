fe878a8e4afc2f6668e84e60ad18136896ddb714
tmilev <todor.milev@gmail.com>
2025-01-05 22:36:17 -0800

    Class Grass

--- bunnies.js
+++ bunnies.js
@@ -10,14 +10,44 @@ function run() {
 }
 art.onload = run;
 
-class Bunny{
+class Grass {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+  draw() {
+    canvas.beginPath();
+    canvas.ellipse(this.x, this.y, 2, 2, 0, 0, 360);
+    canvas.fillStyle = "green";
+    canvas.fill();
+  }
+}
+
+class Bunny {
   constructor() {
     this.x = 0;
     this.y = 0;
   }
   draw() {
     canvas.drawImage(art, 0, 0, 19, 19, this.x, this.y, 40, 40);
-
+  }
+  tick() {
+    let moveX = Math.random() * 6 - 3;
+    let moveY = Math.random() * 6 - 3;
+    this.x += moveX;
+    this.y += moveY;
+    if (
+      this.x > game.canvasWidth ||
+      this.x < 0
+    ) {
+      this.x -= moveX;  
+    }
+    if (
+      this.y > game.canvasHeight ||
+      this.y < 0
+    ) {
+      this.y -= moveY;
+    }
   }
 }
 
@@ -25,30 +55,52 @@ class Game {
   constructor() {
     /** @type {Bunny[]} */
     this.bunnies = [];
+    /** @type {Grass[]} */
+    this.grasses = [];
     this.canvasWidth = canvasContainer.width;
     this.canvasHeight = canvasContainer.height;
     this.bunnyCount = document.getElementById("bunnyCount");
+    this.spawnBunnyButton = document.getElementById("spawnBunny");
+    this.spawnBunnyButton.addEventListener("click", () => { 
+      this.spawnBunny();
+    });
   }
   run() {
     setInterval(() => {
       this.tick();
     }, 100);
   }
+  spawnBunny() {
+    let incomingBunny = new Bunny();
+    incomingBunny.x = Math.floor(Math.random() * (this.canvasWidth - 40));
+    incomingBunny.y = Math.floor(Math.random() * (this.canvasHeight - 40));
+    this.bunnies.push(incomingBunny);
+  }
   tick() {
-    if (this.bunnies.length < 1000) {
-      let incomingBunny = new Bunny();
-      incomingBunny.x = Math.floor(Math.random() * (this.canvasWidth-40));
-      incomingBunny.y = Math.floor(Math.random() * (this.canvasHeight-40));
-      this.bunnies.push(incomingBunny);
+    this.spawnGrass();
+    for (let bunny of this.bunnies) {
+      bunny.tick();
     }
     this.bunnyCount.textContent = this.bunnies.length;
     this.draw();
   }
   draw() {
     canvas.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    for (let grass of this.grasses) {
+      grass.draw();
+    }
     for (let bunny of this.bunnies) {
       bunny.draw();
     }
+  }
+  spawnGrass() {
+    if (this.grasses.length >= 10000) {
+      return;
+    }
+    let newGrass = new Grass();
+    newGrass.x = Math.random() * this.canvasWidth;
+    newGrass.y = Math.random() * this.canvasHeight;
+    this.grasses.push(newGrass);
   }
 }
 
